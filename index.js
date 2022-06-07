48b91aa000d6911e04a9e9dfee6c816967a54594
stellamunson <sftamesis13@gmail.com>
2022-06-07 14:26:55 -0700

    commit tree d1b49210c07ba655863ee6a47e85565b05f7e358

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400) {
+    scuberGreetingForFeet = 'This one is on me!'
+  } else if(someValue > 400 && someValue < 2000){
+    scuberGreetingForFeet = 'That will be twenty bucks.'
+  } else if(someValue > 2000 && someValue < 2500){
+    scuberGreetingForFeet = 'I will gladly take your thirty bucks.'
+  } else if(someValue > 2500){
+    scuberGreetingForFeet = 'No can do.'
+  }
+  return scuberGreetingForFeet
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.';
+ }
 }
+ 
